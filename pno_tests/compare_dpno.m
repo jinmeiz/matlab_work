c2bcaa77eff8810e9041445ea1d0268ab32068a0
Jinmei Zhang <jinmeizhang@cd-it-6983753.dhcp.lbl.gov>
2017-05-03 14:44:36 -0700

    added test code (rank information is still not correct)

--- pno_tests/compare_dpno.m
+++ pno_tests/compare_dpno.m
@@ -1,6 +1,9 @@
 %% compare the subspaces between different PNO coefficients
+% there are six different PNO coefficients
 tic;
 
+%% calculation information
+% H2O/6-31G calculaton: # of occ: 4, # of vir: 8
 mol_name = 'h2o';
 ndocc = 4;
 nvir = 8;
@@ -8,12 +11,17 @@ nvir = 8;
 %% construct PNO coefficients from files
 fprintf('\n constructing PNO coefficients for %s\n', mol_name);
 
-Dab_ij = zeros(nvir,nvir,ndocc,ndocc);
-Dab_ij_new = zeros(nvir,nvir,ndocc,ndocc);
+Dab_ij_tem1 = zeros(nvir,nvir,ndocc,ndocc);
+Dab_ij_tem2 = zeros(nvir,nvir,ndocc,ndocc);
+
+theta_1 = zeros(ndocc,ndocc);
+theta_2 = zeros(ndocc,ndocc);
+idx = 1;
 for iter = 1:3
   fprintf('\n%2dth iteration results: \n',iter-1);
-  for i = 1:ndocc
   
+  Dab_ij_tem2(:,:) = 0;
+  for i = 1:ndocc  
     for j = 1:i
       f_name = strcat(strcat(strcat(strcat(strcat('C_es_', ...
                    int2str(i)),int2str(j)),'_'),int2str(iter-1)),'.out');
@@ -21,19 +29,59 @@ for iter = 1:3
                    int2str(i)),int2str(j)),'_'),int2str(iter-1)),'new.out');
       %fprintf('  reading %10s\n',f_name);
       Dab = load([strcat(strcat('./',mol_name),'/') f_name]); 
-      n_pno = size(Dab,2);
-      Dab_ij(:,1:n_pno,i,j) = Dab;
+      
+      if (iter > 1)
+        % compute the angle between previous and current PNO coefficients
+        theta_2(i,j) = subspace(Dab_ij_tem1(:,:,i,j),Dab);
+%         if (i == 4 && j == 1 && iter == 3)
+%           disp(i);
+%           disp(j);
+%           disp(Dab);
+%           disp(Dab_ij_tem1(:,:,i,j));
+%           disp(theta_2(i,j))
+%         end
+      end
+      
       %fprintf('  reading %10s\n',f_new_name);
       Dab_new = load([strcat(strcat('./',mol_name),'/') f_new_name]); 
-      n_pno_new = size(Dab_new,2);
-      Dab_ij_new(:,1:n_pno_new,i,j) = Dab_new;
       
       % compute the angle between two PNO coefficients
-      theta = subspace(Dab,Dab_new);
-      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %f\n',i,j,n_pno,n_pno_new,theta);
-    end
-  
+      theta_1(i,j) = subspace(Dab,Dab_new);
+      
+      % store the last PNO coefficients
+      Dab_ij_tem2(:,1:size(Dab_new,2),i,j) = Dab_new;
+      
+%       if (i == 4 && j == 2 && iter == 3)
+%         disp(f_name);
+%         disp(f_new_name);
+%         disp(i);
+%         disp(j);
+%         disp(idx);
+%         disp(Dab);
+%         disp(Dab_new);
+%         %disp(Dab_ij(:,1:n_pno,i,j,iter));
+%       end
+    end  
   end
+  Dab_ij_tem1 = Dab_ij_tem2;
+  
+  if (iter > 1)
+  fprintf('\n%2dth comparison (cross iterations): \n',idx);
+  for i = 1:ndocc  
+    for j = 1:i
+      fprintf('  for ij (%d,%d) pair, theta = %f\n',i,j,theta_2(i,j));
+    end
+  end
+  idx = idx+1;
+  end
+  
+  fprintf('\n%2dth comparison: \n',idx);
+  for i = 1:ndocc  
+    for j = 1:i
+      fprintf('  for ij (%d,%d) pair, theta = %f\n',i,j,theta_1(i,j));
+    end
+  end
+  idx = idx+1;
 
 end
 
---
+++ pno_tests/test_com_dpno.m
@@ -0,0 +1,93 @@
+%% compare the subspaces between different PNO coefficients
+% there are six different PNO coefficients
+% this script stores all the PNO coefficients and rank information
+tic;
+
+%% calculation information
+% H2O/6-31G calculaton: # of occ: 4, # of vir: 8
+mol_name = 'h2o';
+bs_name = '6-31G'
+ndocc = 4;
+nvir = 8;
+
+%% construct PNO coefficients from files
+fprintf('\n constructing PNO coefficients for %s %s\n', mol_name,bs_name);
+
+Dab_ij = zeros(nvir,nvir,ndocc,ndocc,3);
+Dab_ij_new = zeros(nvir,nvir,ndocc,ndocc,3);
+n_pno = zeros(ndocc,ndocc,3);
+n_pno_new = zeros(ndocc,ndocc,3);
+
+for iter = 1:3
+ 
+  for i = 1:ndocc  
+    for j = 1:i
+      f_name = strcat(strcat(strcat(strcat(strcat('C_es_', ...
+                   int2str(i)),int2str(j)),'_'),int2str(iter-1)),'.out');
+      f_new_name = strcat(strcat(strcat(strcat(strcat('C_es_', ...
+                   int2str(i)),int2str(j)),'_'),int2str(iter-1)),'new.out');
+      %fprintf('  reading %10s\n',f_name);
+      Dab = load([strcat(strcat('./',mol_name),'/') f_name]); 
+      n_pno(i,j,iter) = size(Dab,2);
+      
+      %fprintf('  reading %10s\n',f_new_name);
+      Dab_new = load([strcat(strcat('./',mol_name),'/') f_new_name]); 
+      n_pno_new(i,j,iter) = size(Dab_new,2);
+           
+      Dab_ij(:,1:n_pno(i,j,iter),i,j,iter) = Dab;  
+      Dab_ij_new(:,1:n_pno_new(i,j,iter),i,j,iter) = Dab_new;
+    end  
+  end
+
+end
+
+toc;
+
+%% compute the subspace between different PNO coefficients
+idx = 1;
+for iter = 1:3
+ 
+if (iter > 1)
+  fprintf('\n%2dth comparison: \n',idx);
+  
+  for i = 1:ndocc  
+    for j = 1:i
+      n_pno_1 = n_pno(i,j,iter);
+      n_pno_2 = n_pno_new(i,j,iter-1);
+      Dab_1 = reshape(Dab_ij(:,:,i,j,iter),nvir,nvir);
+      Dab_2 = reshape(Dab_ij_new(:,:,i,j,iter-1),nvir,nvir);
+      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %f\n',i,j, ...
+        n_pno_1,n_pno_2,subspace(Dab_1(:,1:n_pno_1),Dab_2(:,1:n_pno_2)));
+    end
+  end
+  idx = idx+1;
+end
+
+  fprintf('\n%2dth comparison: \n',idx);
+  for i = 1:ndocc  
+    for j = 1:i
+      n_pno_1 = n_pno(i,j,iter);
+      n_pno_2 = n_pno_new(i,j,iter);
+      Dab_1 = reshape(Dab_ij(:,:,i,j,iter),nvir,nvir);
+      Dab_2 = reshape(Dab_ij_new(:,:,i,j,iter),nvir,nvir);
+      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %f\n',i,j, ...
+        n_pno_1,n_pno_2,subspace(Dab_1(:,1:n_pno_1),Dab_2(:,1:n_pno_2)));
+    end
+  end
+  idx = idx+1;
+end
+
+% Dab_1 = Dab_ij(:,1:3,4,2,3);
+% Dab_2 = Dab_ij_new(:,1:3,4,2,2);
+% disp(Dab_1);
+% disp(Dab_2);
+% fprintf('  for ij (%d,%d) pair, theta = %f\n',4,2,subspace(Dab_2,Dab_1));
+
+%% test
+% Dab_1 = Dab_ij(:,:,4,1,3);
+% Dab_2 = Dab_ij_new(:,:,4,1,2);
+% disp(Dab_1);
+% disp(Dab_2);
+% fprintf('  for ij (%d,%d) pair, theta = %f\n',4,1,subspace(Dab_2,Dab_1));
+
+toc;
