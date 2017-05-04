866ffa0aa4174a4aff3d492ae60da78781b07e42
Jinmei Zhang <jmzhang@vt.edu>
2017-05-04 14:23:54 -0700

    update data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # matlab_work
-MATLAB scripts for work related issues: Cholesky decomposition
+MATLAB scripts for work related issues: 
+* compute_trunc_CD_mat.m: perform Cholesky decomposition and truncate eigenvectors based on eigenvalues
+* overlap_mat: compute the overlap matrix of H2O
+* pno_tests: compute the subpace between different PNO coefficients
--- pno_tests/test_com_dpno.m
+++ pno_tests/test_com_dpno.m
@@ -8,11 +8,11 @@ mol_name = 'h2o';
 ndocc = 4;
 tcut = '1e7';
 % H2O/6-31G calculaton: # of occ: 4, # of vir: 8
-bs_name = '631g';
-nvir = 8;
+% bs_name = '631g';
+% nvir = 8;
 % H2O/aug-cc-pVDZ calculaton: # of occ: 4, # of vir: 36
-% bs_name = 'augdz';
-% nvir = 36;
+bs_name = 'augdz';
+nvir = 36;
 
 %% construct PNO coefficients from files
 fprintf('\n constructing PNO coefficients for %s %s with %s\n', ...
@@ -60,7 +60,7 @@ if (iter > 1)
       n_pno_2 = n_pno_new(i,j,iter-1);
       Dab_1 = reshape(Dab_ij(:,:,i,j,iter),nvir,nvir);
       Dab_2 = reshape(Dab_ij_new(:,:,i,j,iter-1),nvir,nvir);
-      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %f\n',i,j, ...
+      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %e\n',i,j, ...
         n_pno_2,n_pno_1,subspace(Dab_1(:,1:n_pno_1),Dab_2(:,1:n_pno_2)));
     end
   end
@@ -74,12 +74,13 @@ end
       n_pno_2 = n_pno_new(i,j,iter);
       Dab_1 = reshape(Dab_ij(:,:,i,j,iter),nvir,nvir);
       Dab_2 = reshape(Dab_ij_new(:,:,i,j,iter),nvir,nvir);
-      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %f\n',i,j, ...
+      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %e\n',i,j, ...
         n_pno_1,n_pno_2,subspace(Dab_1(:,1:n_pno_1),Dab_2(:,1:n_pno_2)));
     end
   end
   idx = idx+1;
 end
+
 
 % Dab_1 = Dab_ij(:,1:3,4,2,3);
 % Dab_2 = Dab_ij_new(:,1:3,4,2,2);
