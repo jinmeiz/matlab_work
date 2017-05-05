46647be3854ec1ea614023d7f172e376b7225217
Jinmei Zhang <jinmeizhang@cd-it-6983753.dhcp.lbl.gov>
2017-05-05 15:11:36 -0700

    added data for (H2O)2 6-31G

--- pno_tests/test_com_dpno.m
+++ pno_tests/test_com_dpno.m
@@ -4,15 +4,22 @@
 tic;
 
 %% calculation information
-mol_name = 'h2o';
-ndocc = 4;
-tcut = '1e7';
+tcut = '1e10';
+%% H2O
+% mol_name = 'h2o';
+% ndocc = 4;
 % H2O/6-31G calculaton: # of occ: 4, # of vir: 8
 % bs_name = '631g';
 % nvir = 8;
 % H2O/aug-cc-pVDZ calculaton: # of occ: 4, # of vir: 36
-bs_name = 'augdz';
-nvir = 36;
+% bs_name = 'augdz';
+% nvir = 36;
+%% (H2O)2 
+mol_name = 'h2o_2';
+ndocc = 8;
+% (H2O)2/6-31G calculaton: # of occ: 8, # of vir: 16
+bs_name = '631g';
+nvir = 16;
 
 %% construct PNO coefficients from files
 fprintf('\n constructing PNO coefficients for %s %s with %s\n', ...
@@ -60,7 +67,7 @@ if (iter > 1)
       n_pno_2 = n_pno_new(i,j,iter-1);
       Dab_1 = reshape(Dab_ij(:,:,i,j,iter),nvir,nvir);
       Dab_2 = reshape(Dab_ij_new(:,:,i,j,iter-1),nvir,nvir);
-      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %e\n',i,j, ...
+      fprintf('  for ij (%d,%d) pair, rank: %2d vs. %2d, theta = %e\n',i,j, ...
         n_pno_2,n_pno_1,subspace(Dab_1(:,1:n_pno_1),Dab_2(:,1:n_pno_2)));
     end
   end
@@ -74,7 +81,7 @@ end
       n_pno_2 = n_pno_new(i,j,iter);
       Dab_1 = reshape(Dab_ij(:,:,i,j,iter),nvir,nvir);
       Dab_2 = reshape(Dab_ij_new(:,:,i,j,iter),nvir,nvir);
-      fprintf('  for ij (%d,%d) pair, rank: %d vs. %d, theta = %e\n',i,j, ...
+      fprintf('  for ij (%d,%d) pair, rank: %2d vs. %2d, theta = %e\n',i,j, ...
         n_pno_1,n_pno_2,subspace(Dab_1(:,1:n_pno_1),Dab_2(:,1:n_pno_2)));
     end
   end
